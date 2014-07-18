b6fc1f229be2b3c535139db8595fac26608cc9f6
theplanetmike <michaelbowers@Mikebook-Air.local>
2014-07-17 18:51:37 -0700

    Adding new Bootswatch theme

---
+++ application/config/custom_config.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//custom_config.php - a place to save global settings
+
+$config['style'] = 'flatly.css';
+$config['banner'] = 'Default Banner';
+$config['title'] = 'Default Title';
+$config['copyright'] = 'Default Copyright';
+$config['theme'] = '';
